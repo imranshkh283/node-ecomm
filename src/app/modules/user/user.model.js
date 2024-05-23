aa1425bfee845a99dc14bf44ecff95fac1bb8cf7
Shaikh Imran <skimran0306@gmail.com>
2024-05-24 02:46:58 +0530

    imple: pre save bcrypt pass

--- src/app/modules/user/user.model.js
+++ src/app/modules/user/user.model.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-
+const bcrypt = require('bcrypt');
 const userSchema = new mongoose.Schema({
     first_name: {
         type: String,
@@ -45,6 +45,13 @@ const userSchema = new mongoose.Schema({
 });
 
 userSchema.index({ email: 1 });
+
+userSchema.pre('save', async function (next) {
+    // if (this.isModified('password')) {
+    this.password = await bcrypt.hash(this.password, 10);
+    // }
+    next();
+});
 
 const User = mongoose.model('User', userSchema);
 
